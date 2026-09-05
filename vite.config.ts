2e3f64590fd9c9d7190ff343539ed1bbd8cc0492
yunarmedia <164407166+yunarmedia@users.noreply.github.com>
2026-09-05 18:35:44 +0700

    Add Tailwind CSS plugin and path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
 
   plugins: [
     react(),
+    tailwindcss(),
   ],
 
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
   },
 });
